06f5a21b81061dbc2a3b241a828c7b1905251e1c
Fabio Picoli Jr <fabiopicolijr@gmail.com>
2020-05-29 23:25:09 -0300

    add unique categories

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,12 +1,15 @@
-// import { getRepository } from 'typeorm';
-// import path from 'path';
+import { getCustomRepository, getRepository, In, Not } from 'typeorm';
 import fs from 'fs';
 import csvParse from 'csv-parse';
-// import uploadConfig from '../config/upload';
-
-// import AppError from '../errors/AppError';
-
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestCSV {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
   async execute(filePath: string): Promise<Transaction[]> {
@@ -17,6 +20,9 @@ class ImportTransactionsService {
     // para cada registro que passar, criar uma transaction
     // devolver todas as transacoes cadastradas
 
+    const categoryRepository = getRepository(Category);
+    // const transactionRepository = getCustomRepository(Repository);
+
     const contactsReadStream = fs.createReadStream(filePath);
 
     const parsers = csvParse({
@@ -26,8 +32,8 @@ class ImportTransactionsService {
 
     const parseCSV = contactsReadStream.pipe(parsers);
 
-    const transactions = [];
-    const categories = [];
+    const transactions: RequestCSV[] = [];
+    const categoriesCSV: string[] = [];
 
     parseCSV.on('data', async line => {
       const [title, type, value, category] = line.map((cell: string) =>
@@ -36,12 +42,43 @@ class ImportTransactionsService {
 
       if (!title || !type || !value) return;
 
-      categories.push(category);
+      categoriesCSV.push(category);
       transactions.push({ title, type, value, category });
     });
 
     await new Promise(resolve => parseCSV.on('end', resolve));
 
+    // Inicio do foreach de categorias do instrutor
+    const existentCategories = await categoryRepository.find({
+      where: {
+        title: In(categoriesCSV),
+      },
+    });
+
+    // deixando apenas o titulo nas categorias do BD pra ficar mais leve
+    const existentCategoriesTitle = existentCategories.map(
+      (category: Category) => category.title,
+    );
+
+    // filtrando as categorias CSV unicas que nao existem nas categorias do BD
+    const addUniqueCategoryTitles = categoriesCSV
+      .filter(category => !existentCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoryRepository.create(
+      addUniqueCategoryTitles.map(title => ({
+        title,
+      })),
+    );
+    // Fim do foreach de categorias
+
+    // await categoryRepository.save(newCategories);
+
+    console.log({ exists: existentCategoriesTitle });
+
+    console.log({ new: addUniqueCategoryTitles });
+
+    /*
     transactions.sort((a, b) => {
       if (a.type > b.type) {
         return 1;
@@ -51,6 +88,7 @@ class ImportTransactionsService {
       }
       return 0;
     });
+    */
   }
 }
 
